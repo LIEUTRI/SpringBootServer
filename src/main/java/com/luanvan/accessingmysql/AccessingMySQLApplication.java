5b2972ba5ce9991d0bb723fe1e52db7d393266e1
Lieu Minh Tri <lieutri98@gmail.com>
2020-08-08 20:39:25 +0700

    upload file

---
+++ src/main/java/com/luanvan/accessingmysql/AccessingMySQLApplication.java
@@ -0,0 +1,11 @@
+package com.luanvan.accessingmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AccessingMySQLApplication {
+	public static void main(String[] args){
+
+		SpringApplication.run(AccessingMySQLApplication.class, args);
+	}
+}
